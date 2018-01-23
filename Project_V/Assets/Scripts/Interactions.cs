cc7f9482ccbe37f3c02eb2229bbc0cd1b8b4e3da
Harry-Sridge <HlatSridge@outlook.com>
2018-01-22 23:07:04 -0800

    updated

---
+++ Project_V/Assets/Scripts/Interactions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour {
+
+    public Transform playerCamera;
+    public float interactionRange;
+    public LayerMask interactableLayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = new Ray(playerCamera.position, playerCamera.forward);
+        RaycastHit raycastHit;
+
+		if(Physics.Raycast(ray, out raycastHit, interactionRange, interactableLayer))
+        {
+            GameObject hitObject = raycastHit.collider.gameObject;
+            print(hitObject.name);
+        }
+
+        Debug.DrawRay(playerCamera.position, (playerCamera.forward * interactionRange), Color.green);
+	}
+}
